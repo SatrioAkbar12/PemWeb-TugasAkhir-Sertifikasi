5ce33235d066da76b40f30636cc9112f77df3978
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-04 21:49:15 +0700

    migration tabel pendaftar_syarat

---
+++ database/migrations/2021_05_04_143309_create_pendaftar_syarat_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePendaftarSyaratTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendaftar_syarat', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_syarat_sertifikasi');
+            $table->bigInteger('id_pendaftar');
+            $table->string('status_verifikasi_syarat');
+            $table->string('path_bukti');
+            $table->string('verifikasi_asesor');
+            $table->text('komentar_asesor');
+            $table->string('verified_by');
+            $table->timestamp('verified_at');
+            $table->string('created_by');
+            $table->string('edited_by');
+            $table->timestamps();
+
+            $table->foreign('id_syarat_sertifikasi')->references('id')->on('syarat_sertifikasi')->onDelete('cascade');
+            $table->foreign('id_pendaftar')->references('id')->on('pendaftar')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendaftar_syarat');
+    }
+}
