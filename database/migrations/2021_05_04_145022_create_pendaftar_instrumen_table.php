245b41a6bf42ec8555730ed78c8cbaf6d3ff8212
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-04 22:04:04 +0700

    migration tabel pendaftar_instrumen

---
+++ database/migrations/2021_05_04_145022_create_pendaftar_instrumen_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePendaftarInstrumenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendaftar_instrumen', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_pendaftar');
+            $table->bigInteger('id_instrumen_asesmen');
+            $table->text('jawaban_self_asesmen');
+            $table->string('path_bukti');
+            $table->text('komentar_bukti');
+            $table->text('jawaban_asesor_verifikasi');
+            $table->string('verified_by');
+            $table->timestamp('verified_at');
+            $table->string('created_by');
+            $table->string('edited_by');
+            $table->timestamps();
+
+            $table->foreign('id_pendaftar')->references('id')->on('pendaftar')->onDelete('cascade');
+            $table->foreign('id_instrumen_asesmen')->references('id')->on('instrumen_asesmen_kompetensi')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendaftar_instrumen');
+    }
+}
