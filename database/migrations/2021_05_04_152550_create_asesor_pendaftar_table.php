bd03a7e6178ae28fbe1b9f2d578143f36b07710d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-04 22:34:49 +0700

    migration tabel asesor_pendaftar

---
+++ database/migrations/2021_05_04_152550_create_asesor_pendaftar_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsesorPendaftarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asesor_pendaftar', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_asesor_jenis_sertifikasi');
+            $table->bigInteger('id_pendaftar');
+            $table->string('hasil');
+            // $table->timestamps();
+
+            $table->foreign('id_asesor_jenis_sertifikasi')->references('id')->on('asesor_jenis_sertifikasi')->onDelete('cascade');
+            $table->foreign('id_pendaftar')->references('id')->on('pendaftar')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asesor_pendaftar');
+    }
+}
