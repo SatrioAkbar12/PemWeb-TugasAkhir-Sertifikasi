08db2c2a317d298c631ae6ef9d54e97386ffac98
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-04 16:40:23 +0700

    migration tabel syarat_sertifikasi

---
+++ database/migrations/2021_05_04_090128_create_syarat_sertifikasi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSyaratSertifikasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('syarat_sertifikasi', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_ref_jenis_sertifikasi');
+            $table->string('syarat');
+            $table->boolean('is_aktif');
+
+            $table->foreign('id_ref_jenis_sertifikasi')->references('id')->on('ref_jenis_sertifikasi')->onDelete('cascade');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('syarat_sertifikasi');
+    }
+}
