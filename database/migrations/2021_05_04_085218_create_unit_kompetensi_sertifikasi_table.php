e0050d7e7aaf9aec97533231b6031707176d5e0d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-04 16:39:09 +0700

    migration tabel unit_kompetensi_sertifikasi

---
+++ database/migrations/2021_05_04_085218_create_unit_kompetensi_sertifikasi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitKompetensiSertifikasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit_kompetensi_sertifikasi', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_ref_jenis_sertifikasi');
+            $table->bigInteger('id_ref_kompetensi');
+            $table->boolean('is_aktif');
+
+            $table->foreign('id_ref_jenis_sertifikasi')->references('id')->on('ref_jenis_sertifikasi')->onDelete('cascade');
+            $table->foreign('id_ref_kompetensi')->references('id')->on('ref_unit_kompetensi')->onDelete('cascade');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unit_kompetensi_sertifikasi');
+    }
+}
