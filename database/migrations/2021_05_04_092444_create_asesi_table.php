b5c1c5d9fecb3f20e31bc6bd1a8ee7e4050681ab
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-04 16:41:52 +0700

    migration tabel asesi

---
+++ database/migrations/2021_05_04_092444_create_asesi_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsesiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asesi', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim');
+            $table->string('nik');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            // $table-> id ref negara
+            $table->text('alamat');
+            $table->string('no_telepon');
+            $table->string('email');
+            $table->string('kualifikasi_pendidikan');
+            // $table-> id unit
+            $table->timestamps();
+            $table->string('created_by');
+            $table->string('edited_by');
+            $table->bigInteger('id_user');
+
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asesi');
+    }
+}
