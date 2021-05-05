342983ef5add9be0fb5e7a1a80925982c375a83f
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-05 14:09:02 +0700

    migration tabel jadwal

---
+++ database/migrations/2021_05_05_065335_create_jadwal_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJadwalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_penawaran_sertifikasi');
+            $table->bigInteger('id_jadwal');
+            $table->date('tanggal_awal');
+            $table->date('tanggal_akhir');
+            $table->timestamp('created_at');
+            $table->string('created_by');
+            $table->boolean('is_show');
+            $table->text('deskripsi');
+
+            $table->foreign('id_penawaran_sertifikasi')->references('id')->on('penawaran_sertifikasi')->onDelete('cascade');
+            $table->foreign('id_jadwal')->references('id')->on('ref_kegiatan')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jadwal');
+    }
+}
