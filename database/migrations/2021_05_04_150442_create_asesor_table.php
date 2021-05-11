d7fa41e8b38a348976ea0607fc54cd2fb530cc9a
sir-shalahuddin <sirajshalahuddin@gmail.com>
2021-05-11 10:16:23 +0700

    php

--- database/migrations/2021_05_04_150442_create_asesor_table.php
+++ database/migrations/2021_05_04_150442_create_asesor_table.php
@@ -16,7 +16,20 @@ class CreateAsesorTable extends Migration
         Schema::create('asesor', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-
+            $table->string('nip');
+            $table->string('nik');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            $table->unsignedBigInteger('id_ref_negara');
+            $table->text('alamat');
+            $table->string('no_telepon');
+            $table->string('email');
+            $table->string('kualifikasi_pendidikan');
+            $table->unsignedBigInteger('id_prodi');
+            $table->timestamps();
+            $table->string('created_by');
+            $table->string('edited_by');
             $table->unsignedBigInteger('id_user');
             // $table->timestamps();
         });
---
+++ database/migrations/2021_05_11_021853_create_prodi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prodi', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('fakultas')->nullable();
+            $table->timestamps();
+            $table->string('created_by');
+            $table->string('edited_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prodi');
+    }
+}
