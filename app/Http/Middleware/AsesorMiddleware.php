3fc03b8000d0255b67edaf1cbde2325c42cd227c
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-17 04:19:22 +0700

    add AdminMiddleware dan AsesorMiddleware, assign global middleware

---
+++ app/Http/Middleware/AsesorMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AsesorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = User::find(Auth::id());
+
+        if($user->role == 'aseosr' || $user->role == 'admin'){
+            return $next($request);
+        }
+        abort('403', 'hanya khusus asesor');
+    }
+}
