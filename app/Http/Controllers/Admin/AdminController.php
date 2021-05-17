0ab0bb40d389d547f74a8f9d222d31f6c757fe3d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-17 14:20:28 +0700

    add AdminController, AsesiController, AsesorController

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+
+class AdminController extends Controller
+{
+    public function show() {
+        $data = User::where('role', 'admin')->get();
+        return view('admin.admin.show', ['data' => $data]);
+    }
+
+    public function showCreate() {
+        return view('admin.admin.form_tambah_data');
+    }
+
+    public function create(Request $request) {
+        $request->validate([
+            'username' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|required_with:password_confirmation|same:password_confirmation',
+            'password_confirmation' => 'required|min:8',
+        ]);
+
+        User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'admin'
+        ]);
+
+        return redirect('/admin/kelola-admin');
+    }
+
+    public function read($id) {
+        $data = User::find($id);
+
+        return view('admin.admin.show_data', ['data' => $data ]);
+    }
+
+    public function showEdit() {
+        $data = Auth::user();
+
+        // return view('admin.admin.form_edit_data', ['data' => $data ]);
+        return 'tes';
+    }
+
+    public function edit(Request $request) {
+        $request->validate([
+            'id' => 'required',
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
+
+        User::where('id', $request->id)->update([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect('/admin/kelola-admin');
+    }
+
+    public function del($id) {
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect('/admin/kelola-admin');
+    }
+}
