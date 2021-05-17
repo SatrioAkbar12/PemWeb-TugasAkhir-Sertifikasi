3a8ac9b41942b254fe4b011b6e5dcc19281d41e6
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-17 12:13:07 +0700

    update migration

---
+++ database/migrations/2021_05_17_045140_alter_drop_collumn_asesi_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterDropCollumnAsesiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('asesi', function (Blueprint $table) {
+            $table->string('nama');
+            $table->dropColumn('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asesi');
+    }
+}
