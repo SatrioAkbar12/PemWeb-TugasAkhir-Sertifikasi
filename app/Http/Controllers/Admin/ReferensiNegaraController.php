5ccec4ff300eb1a3da74a9270cd4f32df312eefb
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-17 09:21:28 +0700

    add model RefNegara, update controller untuk auth, update controller Admin/ReferensiNegaraController.php

--- app/Http/Controllers/Admin/ReferensiNegaraController.php
+++ app/Http/Controllers/Admin/ReferensiNegaraController.php
@@ -4,10 +4,59 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\RefNegara;
 
 class ReferensiNegaraController extends Controller
 {
     public function show() {
-        return view('admin.referensi_negara.show');
+        $data = RefNegara::all();
+        return view('admin.referensi_negara.show', ['data' => $data]);
+    }
+
+    public function showCreate() {
+        return view('admin.referensi_negara.form_tambah_data');
+    }
+
+    public function create(Request $request) {
+        $this->validate($request,[
+    		'nama' => 'required'
+    	]);
+
+        RefNegara::create([
+            'nama' => $request->nama
+        ]);
+
+        return redirect('/admin/negara');
+    }
+
+    public function read($id) {
+        $data = RefNegara::find($id);
+
+        return view('admin.referensi_negara.show_data', ['data' => $data]);
+    }
+
+    public function showEdit($id) {
+        $data = RefNegara::find($id);
+
+        return view('admin.referensi_negara.form_edit_data', ['data' => $data]);
+    }
+
+    public function edit(Request $request) {
+        $this->validate($request,[
+    		'nama' => 'required'
+    	]);
+
+        RefNegara::where('id', $request->id)->update([
+            'nama'=> $request->nama
+        ]);
+
+        return redirect('/admin/negara');
+    }
+
+    public function del($id) {
+        $data = RefNegara::find($id);
+        $data->delete();
+
+        return redirect('/admin/negara');
     }
 }
--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,16 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+
+        if(Auth::user()->role == 'admin') {
+            return redirect('/admin');
+        }
+        elseif(Auth::user()->role == 'asesor') {
+            return redirect('/asesor');
+        }
+        else {
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
     }
 
     /**
