de41ae855dd24d407ac3127d7dd631388b1c0a57
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-17 10:56:20 +0700

    update template dashboard admin

--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -43,8 +43,8 @@
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="info">
                     <p class="text-light">
-                        @yield('nama')<br>
-                        @yield('role')
+                        {{ Auth::user()->nama }}<br>
+                        {{ Auth::user()->role }}
                     </p>
                 </div>
             </div>
