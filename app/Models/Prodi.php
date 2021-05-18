688e07519999792e4492f54e4b3e8270b54202e1
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-18 16:06:36 +0700

    add model Prodi

---
+++ app/Models/Prodi.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prodi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prodi';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = 'true';
+
+    protected $fillable = [
+        'nama',
+        'fakultas',
+        'created_by',
+        'edited_by'
+    ];
+}
