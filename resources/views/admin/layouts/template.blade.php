a8ecbfeb73badc5e39f7dcf581a322cb732a5906
Nisa Zahir <nisa24.bz@student.uns.ac.id>
2021-05-30 09:08:56 +0700

    Tambah error message buat semua form

--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -114,6 +114,17 @@
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
         @yield('content')
     </div>
     <!-- /.content-wrapper -->
