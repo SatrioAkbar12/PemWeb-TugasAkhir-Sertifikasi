062029074bac26ff3fc2d02bad7755ff7c705243
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-05-31 11:11:58 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AdminDashboardController;
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\AsesiController;
-//use App\Http\Controllers\Admin\AsesorController;
-use App\Http\Controllers\Admin\KegiatanController;
-use App\Http\Controllers\Admin\ProdiController;
-use App\Http\Controllers\Admin\ReferensiNegaraController;
-use App\Http\Controllers\Asesor\AsesorDashboardController;
-use App\Http\Controllers\Asesor\AsesorController;
 
+//!!!!!!!!!!!!!!!!!!!!!! Controller jangan lupa diberi as untuk nama lain !!!!!!!!!!!!!!!!!!!!!!//
+use App\Http\Controllers\Admin\AdminDashboardController as admin_AdminDashboardController;
+use App\Http\Controllers\Admin\AdminController as admin_AdminController;
+use App\Http\Controllers\Admin\AsesiController as admin_AsesiController;
+use App\Http\Controllers\Admin\AsesorController as admin_AsesorController;
+use App\Http\Controllers\Admin\KegiatanController as admin_KegiatanController;
+use App\Http\Controllers\Admin\ProdiController as admin_ProdiController;
+use App\Http\Controllers\Admin\ReferensiNegaraController as admin_ReferensiNegaraController;
+use App\Http\Controllers\Asesor\AsesorDashboardController as asesor_AsesorDashboardController;
+use App\Http\Controllers\Asesor\AsesorController as asesor_AsesorController;
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,91 +34,88 @@ Route::get('/dashboard', function () {
 
 // Route khusus admin
 Route::prefix('admin')->middleware('auth', 'admin')->group(function () {
-    Route::get('/', [AdminDashboardController::class, 'showDashboard']);
+    Route::get('/', [admin_AdminDashboardController::class, 'showDashboard']);
 
     Route::prefix('kelola-admin')->group(function() {
-        Route::get('/', [AdminController::class, 'show']);
-        Route::get('/tambah', [AdminController::class, 'showCreate']);
-        Route::post('/tambah', [AdminController::class, 'create']);
-        Route::get('/edit', [AdminController::class, 'showEdit']);
-        Route::put('/edit', [AdminController::class, 'edit']);
-        Route::get('/edit/ganti-password', [AdminController::class, 'showChangePassword']);
-        Route::put('/edit/ganti-password', [AdminController::class, 'changePassword']);
-        Route::get('/{id}', [AdminController::class, 'read']);
-        Route::get('/{id}/delete', [AdminController::class, 'del']);
+        Route::get('/', [admin_AdminController::class, 'show']);
+        Route::get('/tambah', [admin_AdminController::class, 'showCreate']);
+        Route::post('/tambah', [admin_AdminController::class, 'create']);
+        Route::get('/edit', [admin_AdminController::class, 'showEdit']);
+        Route::put('/edit', [admin_AdminController::class, 'edit']);
+        Route::get('/edit/ganti-password', [admin_AdminController::class, 'showChangePassword']);
+        Route::put('/edit/ganti-password', [admin_AdminController::class, 'changePassword']);
+        Route::get('/{id}', [admin_AdminController::class, 'read']);
+        Route::get('/{id}/delete', [admin_AdminController::class, 'del']);
     });
 
     Route::prefix('kelola-asesi')->group(function() {
-        Route::get('/', [AsesiController::class, 'show']);
-        Route::get('/tambah', [AsesiController::class, 'showCreate']);
-        Route::post('/tambah', [AsesiController::class, 'create']);
-        Route::get('/{id}', [AsesiController::class, 'read']);
-        Route::get('/{id}/edit', [AsesiController::class, 'showEdit']);
-        Route::put('/{id}/edit', [AsesiController::class, 'edit']);
-        Route::get('/{id}/delete', [AsesiController::class, 'del']);
+        Route::get('/', [admin_AsesiController::class, 'show']);
+        Route::get('/tambah', [admin_AsesiController::class, 'showCreate']);
+        Route::post('/tambah', [admin_AsesiController::class, 'create']);
+        Route::get('/{id}', [admin_AsesiController::class, 'read']);
+        Route::get('/{id}/edit', [admin_AsesiController::class, 'showEdit']);
+        Route::put('/{id}/edit', [admin_AsesiController::class, 'edit']);
+        Route::get('/{id}/delete', [admin_AsesiController::class, 'del']);
     });
 
     Route::prefix('kelola-asesor')->group(function() {
-        Route::get('/', [AsesorController::class, 'show']);
-        Route::get('/tambah', [AsesorController::class, 'showCreate']);
-        Route::post('/tambah', [AsesorController::class, 'create']);
-        Route::get('/{id}', [AsesorController::class, 'read']);
-        Route::get('/{id}/edit', [AsesorController::class, 'showEdit']);
-        Route::put('/{id}/edit', [AsesorController::class, 'edit']);
-        Route::get('/{id}/delete', [AsesorController::class, 'del']);
+        Route::get('/', [admin_AsesorController::class, 'show']);
+        Route::get('/tambah', [admin_AsesorController::class, 'showCreate']);
+        Route::post('/tambah', [admin_AsesorController::class, 'create']);
+        Route::get('/{id}', [admin_AsesorController::class, 'read']);
+        Route::get('/{id}/edit', [admin_AsesorController::class, 'showEdit']);
+        Route::put('/{id}/edit', [admin_AsesorController::class, 'edit']);
+        Route::get('/{id}/delete', [admin_AsesorController::class, 'del']);
     });
 
     Route::prefix('negara')->group(function () {
-        Route::get('/', [ReferensiNegaraController::class, 'show']);
-        Route::get('/tambah', [ReferensiNegaraController::class, 'showCreate']);
-        Route::post('/tambah', [ReferensiNegaraController::class, 'create']);
-        Route::get('/{id}', [ReferensiNegaraController::class, 'read']);
-        Route::get('/{id}/edit', [ReferensiNegaraController::class, 'showEdit']);
-        Route::put('/{id}/edit', [ReferensiNegaraController::class, 'edit']);
-        Route::get('/{id}/delete', [ReferensiNegaraController::class, 'del']);
+        Route::get('/', [admin_ReferensiNegaraController::class, 'show']);
+        Route::get('/tambah', [admin_ReferensiNegaraController::class, 'showCreate']);
+        Route::post('/tambah', [admin_ReferensiNegaraController::class, 'create']);
+        Route::get('/{id}', [admin_ReferensiNegaraController::class, 'read']);
+        Route::get('/{id}/edit', [admin_ReferensiNegaraController::class, 'showEdit']);
+        Route::put('/{id}/edit', [admin_ReferensiNegaraController::class, 'edit']);
+        Route::get('/{id}/delete', [admin_ReferensiNegaraController::class, 'del']);
     });
 
     Route::prefix('prodi')->group(function() {
-        Route::get('/', [ProdiController::class, 'show']);
-        Route::get('/tambah', [ProdiController::class, 'showCreate']);
-        Route::post('/tambah', [ProdiController::class, 'create']);
-        Route::get('/{id}', [ProdiController::class, 'read']);
-        Route::get('/{id}/edit', [ProdiController::class, 'showEdit']);
-        Route::put('/{id}/edit', [ProdiController::class, 'edit']);
-        Route::get('/{id}/delete', [ProdiController::class, 'del']);
+        Route::get('/', [admin_ProdiController::class, 'show']);
+        Route::get('/tambah', [admin_ProdiController::class, 'showCreate']);
+        Route::post('/tambah', [admin_ProdiController::class, 'create']);
+        Route::get('/{id}', [admin_ProdiController::class, 'read']);
+        Route::get('/{id}/edit', [admin_ProdiController::class, 'showEdit']);
+        Route::put('/{id}/edit', [admin_ProdiController::class, 'edit']);
+        Route::get('/{id}/delete', [admin_ProdiController::class, 'del']);
     });
 
     Route::prefix('kegiatan')->group(function() {
-        Route::get('/', [KegiatanController::class, 'index']);
-        Route::get('/tambah', [KegiatanController::class, 'showCreate']);
-        Route::post('/tambah', [KegiatanController::class, 'create']);
-        Route::get('/{id}', [KegiatanController::class, 'read']);
-        Route::get('/{id}/edit', [KegiatanController::class, 'showEdit']);
-        Route::put('/{id}/edit', [KegiatanController::class, 'edit']);
-        Route::get('/{id}/delete', [KegiatanController::class, 'del']);
+        Route::get('/', [admin_KegiatanController::class, 'index']);
+        Route::get('/tambah', [admin_KegiatanController::class, 'showCreate']);
+        Route::post('/tambah', [admin_KegiatanController::class, 'create']);
+        Route::get('/{id}', [admin_KegiatanController::class, 'read']);
+        Route::get('/{id}/edit', [admin_KegiatanController::class, 'showEdit']);
+        Route::put('/{id}/edit', [admin_KegiatanController::class, 'edit']);
+        Route::get('/{id}/delete', [admin_KegiatanController::class, 'del']);
     });
 });
 
 // Route khusus asesor
 Route::prefix('asesor')->middleware('auth', 'asesor')->group(function () {
-    Route::get('/', [AsesorDashboardController::class, 'showDashboard']);
-    // Route::get('/', function () {
-    // //     return view('asesor.index');
-    // });
+    Route::get('/', [asesor_AsesorDashboardController::class, 'showDashboard']);
 
     Route::prefix('lihat-data')->group(function() {
-        Route::get('/', [AsesorController::class, 'show']);
-        // Route::get('/tambah', [AsesorController::class, 'showCreate']);
-        // Route::post('/tambah', [AsesorController::class, 'create']);
-       // Route::get('/{id}', [AsesorController::class, 'read']);
-        // Route::get('/{id}/edit', [AsesorController::class, 'showEdit']);
-        // Route::put('/{id}/edit', [AsesorController::class, 'edit']);
-        // Route::get('/{id}/delete', [AsesorController::class, 'del']);
+        Route::get('/', [asesor_AsesorController::class, 'show']);
+        // Route::get('/tambah', [asesor_AsesorController::class, 'showCreate']);
+        // Route::post('/tambah', [asesor_AsesorController::class, 'create']);
+       // Route::get('/{id}', [asesor_AsesorController::class, 'read']);
+        // Route::get('/{id}/edit', [asesor_AsesorController::class, 'showEdit']);
+        // Route::put('/{id}/edit', [asesor_AsesorController::class, 'edit']);
+        // Route::get('/{id}/delete', [asesor_AsesorController::class, 'del']);
     });
 
     Route::prefix('edit-data')->group(function() {
-        Route::get('/', [AsesorController::class, 'showEdit']);
-        Route::put('/edit', [AsesorController::class, 'edit']);
+        Route::get('/', [asesor_AsesorController::class, 'showEdit']);
+        Route::put('/edit', [asesor_AsesorController::class, 'edit']);
 
     });
 });
