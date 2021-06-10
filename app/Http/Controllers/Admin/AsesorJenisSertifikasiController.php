09f91691277836dd58ad087674d087c4a55fa70c
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-06-10 23:05:51 +0700

    add AsesorJenisSertifikasiController admin

---
+++ app/Http/Controllers/Admin/AsesorJenisSertifikasiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class AsesorJenisSertifikasiController extends Controller
+{
+    public function index() {
+
+    }
+
+    public function read($id) {
+
+    }
+
+    public function showEdit($id) {
+
+    }
+
+    public function edit($id, Request $request) {
+
+    }
+}
