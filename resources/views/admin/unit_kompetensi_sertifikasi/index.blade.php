fe37de3058bba796f31ce19f5ae00b6ec937eac4
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-06-11 16:56:06 +0700

    add view admin kelola unit kompetensi sertifikasi

---
+++ resources/views/admin/unit_kompetensi_sertifikasi/index.blade.php
@@ -0,0 +1,85 @@
+@extends('admin.layouts.template')
+
+@section('head_title', 'Unit Kompetensi Sertifikasi')
+
+@section('sidebar-unitKompetensiSertifikasi-active', 'active')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+            <h1>Unit Kompetensi Sertifikasi</h1>
+            </div>
+        </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+<!-- Main content -->
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+      <div class="card-header">
+        *jika jenis sertifikasi tidak muncul, kemungkinan belum ada unit kompetensi sertifikasi yang ditambahkan
+        <div class="card-tools">
+            <a href="/admin/unit-kompetensi-sertifikasi/tambah">
+                <button type="button" class="btn btn-success">Tambah Data</button>
+            </a>
+        </div>
+      </div>
+      <div class="card-body p-0">
+        <table class="table table-striped projects">
+            <thead class="text-center">
+                <tr>
+                    <th>
+                        ID Jenis Sertifikasi
+                    </th>
+                    <th>
+                        Jenis Sertifikasi
+                    </th>
+                    <th>
+                    </th>
+                </tr>
+            </thead>
+            @foreach ($data as $d)
+            <tbody>
+                <tr>
+                    <td class="text-center">
+                        {{ $d->refJenisSertifikasi->id }}
+                    </td>
+                    <td>
+                        <a>
+                            {{ $d->refJenisSertifikasi->nama }}
+                        </a>
+                    </td>
+                    <td class="project-actions text-right">
+                        <a class="btn btn-primary btn-sm" href="/admin/unit-kompetensi-sertifikasi/{{ $d->refJenisSertifikasi->id }}">
+                            <i class="fas fa-folder">
+                            </i>
+                            View
+                        </a>
+                        {{-- <a class="btn btn-info btn-sm" href="/admin/unit-kompetensi-sertifikasi/{{ $d->id }}/edit">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a> --}}
+                        {{-- <a class="btn btn-danger btn-sm" href="/admin/unit-kompetensi-sertifikasi/{{ $d->id }}/delete">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a> --}}
+                    </td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+
+  </section>
+  <!-- /.content -->
+@endsection
