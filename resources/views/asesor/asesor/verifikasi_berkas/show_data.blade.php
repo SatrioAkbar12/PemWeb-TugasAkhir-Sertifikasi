5dd57effaa77b99751b8d944ca647230928b1430
xxsimpnaton git <nikolatief11@gmail.com>
2021-06-14 01:27:16 +0700

    create index verifikasi_berkas

---
+++ resources/views/asesor/asesor/verifikasi_berkas/show_data.blade.php
@@ -0,0 +1,68 @@
+@extends('asesor.layouts.template')
+
+@section('head_title', 'Asesor')
+
+@section('sidebar-prodi-active', 'active')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Peserta Sertifikasi</h1>
+                </div>
+            </div>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-body p-0">
+                <table class="table table-striped projects">
+                    <thead class="text-center">
+                        <tr>
+                            <th>
+                                ID
+                            </th>
+                            <th>
+                                Nama Peserta
+                            </th>
+                            <th>
+                            </th>
+                        </tr>
+                    </thead>
+                    @foreach ($data_pendaftar as $d)
+                        <tbody>
+                            <tr>
+                                <td class="text-center">
+                                    {{ $d->pendaftar->asesi->id }}
+                                </td>
+                                <td class="text-center">
+                                    <a>
+                                        {{ $d->pendaftar->asesi->nama }}
+                                    </a>
+                                </td>
+                                <td class="project-actions text-right">
+                                    <a class="btn btn-primary btn-sm" href="/admin/kelola-asesor/{{ $d->id }}">
+                                        <i class="fas fa-folder">
+                                        </i>
+                                        View
+                                    </a>
+                                    <a class="btn btn-info btn-sm" href="/admin/kelola-asesor/{{ $d->id }}/edit">
+                                        <i class="fas fa-pencil-alt">
+                                        </i>
+                                        Verifikasi
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+
+
+@endsection
