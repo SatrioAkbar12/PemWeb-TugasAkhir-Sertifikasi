e4cb67bc91fbdea9848310de493d41152995e53a
xxsimpnaton git <nikolatief11@gmail.com>
2021-06-15 00:54:16 +0700

    create show_data verifikasi-berkas

--- resources/views/asesor/asesor/verifikasi_berkas/show_data.blade.php
+++ resources/views/asesor/asesor/verifikasi_berkas/show_data.blade.php
@@ -10,59 +10,69 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Peserta Sertifikasi</h1>
+                    <h1>Detail Peserta Sertifikasi</h1>
                 </div>
             </div>
     </section>
 
-    <!-- Main content -->
     <section class="content">
-
         <!-- Default box -->
         <div class="card">
-            <div class="card-body p-0">
-                <table class="table table-striped projects">
-                    <thead class="text-center">
-                        <tr>
-                            <th>
-                                ID
-                            </th>
-                            <th>
-                                Nama Peserta
-                            </th>
-                            <th>
-                            </th>
-                        </tr>
-                    </thead>
-                    @foreach ($data_pendaftar as $d)
-                        <tbody>
-                            <tr>
-                                <td class="text-center">
-                                    {{ $d->pendaftar->asesi->id }}
-                                </td>
-                                <td class="text-center">
-                                    <a>
-                                        {{ $d->pendaftar->asesi->nama }}
-                                    </a>
-                                </td>
-                                <td class="project-actions text-right">
-                                    <a class="btn btn-primary btn-sm" href="/admin/kelola-asesor/{{ $d->id }}">
-                                        <i class="fas fa-folder">
-                                        </i>
-                                        View
-                                    </a>
-                                    <a class="btn btn-info btn-sm" href="/admin/kelola-asesor/{{ $d->id }}/edit">
-                                        <i class="fas fa-pencil-alt">
-                                        </i>
-                                        Verifikasi
-                                    </a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    @endforeach
-                </table>
+            <div class="card-body p-4">
+                <div class="row">
+                    <div class="col-2">
+                        ID
+                    </div>
+                    <div class="col-10">
+                        : {{ $data->pendaftar->asesi->id }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Nama
+                    </div>
+                    <div class="col-10">
+                        : {{ $data->pendaftar->asesi->nama }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        No telepon
+                    </div>
+                    <div class="col-10">
+                        : {{ $data->pendaftar->asesi->no_telepon }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Jenis Kelamin
+                    </div>
+                    <div class="col-10">
+                        : {{ $data->pendaftar->asesi->jenis_kelamin }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Nim
+                    </div>
+                    <div class="col-10">
+                        : {{ $data->pendaftar->asesi->nim }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Nik
+                    </div>
+                    <div class="col-10">
+                        : {{ $data->pendaftar->asesi->nik }}
+                    </div>
+                </div>
+                
+                <br>
+                <a href="/asesor/verifikasi-berkas/">
+                    <button type="button" class="btn btn-danger">Kembali</button>
+                </a>
             </div>
         </div>
-
-
+    </section>
 @endsection
