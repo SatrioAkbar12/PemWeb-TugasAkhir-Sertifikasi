bb48b545a30175982d5b005d217460dadea90b09
SatrioAkbar12 <65120345+SatrioAkbar12@users.noreply.github.com>
2021-06-27 15:08:47 +0700

    update Asesi\AsesiSelfAsesmenController (update pengerjaan dan akhiri ujian)

--- app/Http/Controllers/Asesi/AsesiSelfAsesmenController.php
+++ app/Http/Controllers/Asesi/AsesiSelfAsesmenController.php
@@ -11,6 +11,7 @@ use App\Models\PendaftarSyarat;
 use App\Models\UnitKompetensiSertifikasi;
 use App\Models\RefJenisSertifikasi;
 use App\Models\RefUnitKompetensi;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -35,78 +36,133 @@ class AsesiSelfAsesmenController extends Controller
     public function showView($id_sertifikasi)
     {
         $data = PenawaranSertifikasi::find($id_sertifikasi);
-        $data_kompetensi = UnitKompetensiSertifikasi::where('id_ref_jenis_sertifikasi', $data->id_ref_jenis_sertifikasi)->get();
-
-        return view('asesi.selfAsesmen.showView', ['data' => $data, 'data_kompetensi' => $data_kompetensi]);
+        $data_kompetensi = UnitKompetensiSertifikasi::where([
+            'id_ref_jenis_sertifikasi' => $data->id_ref_jenis_sertifikasi,
+            'is_aktif' => 1
+        ])->get();
+        $data_pendaftarinstrumen = PendaftarInstrumen::where('id_pendaftar', $this->getPendaftar($id_sertifikasi)->id)->get();
+        // dd($data_pendaftarinstrumen);
+        return view('asesi.selfAsesmen.showView', ['data' => $data, 'data_kompetensi' => $data_kompetensi, 'data_pendaftarinstrumen' => $data_pendaftarinstrumen]);
     }
 
     public function showUnitKompetensi($id_sertifikasi, $id_ref_unit_kompetensi)
     {
-        $data = RefUnitKompetensi::find($id_ref_unit_kompetensi);
+        $pendaftar = $this->getPendaftar($id_sertifikasi);
+        $data_refunitkompetensi = RefUnitKompetensi::find($id_ref_unit_kompetensi);
+        $data = PendaftarInstrumen::where('id_pendaftar', $pendaftar->id)->get();
 
-        return view('asesi.selfAsesmen.show_unit_kompetensi', ['data' => $data, 'id_sertifikasi' => $id_sertifikasi]);
+        return view('asesi.selfAsesmen.show_unit_kompetensi', ['data' => $data, 'id_sertifikasi' => $id_sertifikasi, 'data_refunitkompetensi' => $data_refunitkompetensi]);
     }
 
-    public function lockUnitKompetensi($id_sertifikasi, $id_ref_unit_kompetensi) {
-        //Lock jawaban unit kompetensi
+    public function storeAsesmen(Request $request, $id_sertifikasi, $id_ref_unit_kompetensi) {
+        $pendaftar = $this->getPendaftar($id_sertifikasi);
+        $instrumen_asesmen = InstrumenAsesmenKompetensi::where('id_ref_unit_kompetensi', $id_ref_unit_kompetensi)->get();
+
+        foreach($instrumen_asesmen as $instrumen) {
+            $jawaban = $instrumen->id."_jawaban";
+            $upload = $instrumen->id."_upload";
+            $uploadkomentar = $instrumen->id."_uploadkomentar";
+
+            $file = $request->file($upload);
+            $path = null;
+
+            if($file != null) {
+                $nama_file = $file->getClientOriginalName();
+                $tujuan_upload = 'data_asesi'.Auth::user()->username.'/'.$id_sertifikasi.'/'.$pendaftar->id.'/asesmen/'.$instrumen->id;
+                $path = $tujuan_upload.'/'.$nama_file;
+
+                $file->move($tujuan_upload, $nama_file);
+            }
+
+            PendaftarInstrumen::where([
+                'id_pendaftar' => $pendaftar->id,
+                'id_instrumen_asesmen' => $instrumen->id
+            ])->update([
+                'jawaban_self_asesmen' => $request->$jawaban,
+                'path_bukti' => $path,
+                'komentar_bukti' => $request->$uploadkomentar,
+                'edited_by' => Auth::user()->username
+            ]);
+        }
+
+        return redirect()->route('asesi.self-asesmen.show-view', ['id_sertifikasi' => $id_sertifikasi]);
     }
 
-    public function showJawab($id_sertifikasi, $id_ref_unit_kompetensi, $id_instrumen_asesmen) {
-        $data = InstrumenAsesmenKompetensi::find($id_instrumen_asesmen);
+    public function akhiriAsesmen($id_sertifikasi) {
+        $pendaftar = $this->getPendaftar($id_sertifikasi);
 
-        return view('asesi.selfAsesmen.form_jawab', ['data' => $data, 'id_sertifikasi' => $id_sertifikasi, 'id_ref_unit_kompetensi' => $id_ref_unit_kompetensi]);
-    }
-
-    public function jawab(Request $request, $id_sertifikasi, $id_ref_unit_kompetensi, $id_instrumen_asesmen)
-    {
-        $request->validate([
-            'jawaban' => 'required'
+        Pendaftar::where('id', $pendaftar->id)->update([
+            'status_akhir_sertifikasi' => 'selesai asemen'
         ]);
 
-        $username = Auth::user()->username;
-        $instrumen = InstrumenAsesmenKompetensi::find($id_instrumen_asesmen);
+        return redirect()->route('asesi.self-asesmen.index');
+    }
+
+    // public function showJawab($id_sertifikasi, $id_ref_unit_kompetensi, $id_instrumen_asesmen) {
+    //     $data = InstrumenAsesmenKompetensi::find($id_instrumen_asesmen);
+
+    //     return view('asesi.selfAsesmen.form_jawab', ['data' => $data, 'id_sertifikasi' => $id_sertifikasi, 'id_ref_unit_kompetensi' => $id_ref_unit_kompetensi]);
+    // }
+
+    // public function jawab(Request $request, $id_sertifikasi, $id_ref_unit_kompetensi, $id_instrumen_asesmen)
+    // {
+    //     $request->validate([
+    //         'jawaban' => 'required'
+    //     ]);
+
+    //     $username = Auth::user()->username;
+    //     $instrumen = InstrumenAsesmenKompetensi::find($id_instrumen_asesmen);
+    //     $pendaftar = Pendaftar::where([
+    //         'id_asesi' => Auth::user()->asesi->id,
+    //         'id_penawaran_sertifikasi' => $id_sertifikasi
+    //     ])->first();
+
+    //     $path_file = null;
+
+    //     if($request->file != null) {
+    //         $file = $request->file('file');
+
+    //         $nama_file = $file->getClientOriginalName();
+    //         $tujuan_upload = 'data_asesi/'.$username.'/'.$id_sertifikasi.'/'.$pendaftar->id.'/asesmen/'.$instrumen->id;
+    //         $path_file = $tujuan_upload.'/'.$nama_file;
+
+    //         $file->move($tujuan_upload, $nama_file);
+    //     }
+
+    //     $data_pendaftarInstrumen = PendaftarInstrumen::where([
+    //         'id_pendaftar' => $pendaftar->id,
+    //         'id_instrumen_asesmen' => $id_instrumen_asesmen
+    //     ])->first();
+
+    //     if($data_pendaftarInstrumen == null) {
+    //         PendaftarInstrumen::create([
+    //             'id_pendaftar' => $pendaftar->id,
+    //             'id_instrumen_asesmen' => $id_instrumen_asesmen,
+    //             'jawaban_self_asesmen' => $request->jawaban,
+    //             'path_bukti' => $path_file,
+    //             'komentar_bukti' => $request->komentar,
+    //             'created_by' => Auth::user()->username,
+    //             'edited_by' => Auth::user()->username
+    //         ]);
+    //     }
+    //     else {
+    //         PendaftarInstrumen::where('id', $data_pendaftarInstrumen->id)->update([
+    //             'jawaban_self_asesmen' => $request->jawaban,
+    //             'path_bukti' => $path_file,
+    //             'komentar_bukti' => $request->komentar,
+    //             'edited_by' => Auth::user()->username
+    //         ]);
+    //     }
+
+    //     return redirect()->route('asesi.self-asesmen.show-unit-kompetensi', ['id_sertifikasi' => $id_sertifikasi, 'id_ref_unit_kompetensi' => $id_ref_unit_kompetensi]);
+    // }
+
+    public function getPendaftar($id_sertifikasi) {
         $pendaftar = Pendaftar::where([
             'id_asesi' => Auth::user()->asesi->id,
             'id_penawaran_sertifikasi' => $id_sertifikasi
         ])->first();
 
-        $path_file = null;
-
-        if($request->file != null) {
-            $file = $request->file('file');
-
-            $nama_file = $file->getClientOriginalName();
-            $tujuan_upload = 'data_asesi/'.$username.'/'.$id_sertifikasi.'/'.$pendaftar->id.'/asesmen/'.$instrumen->id;
-            $path_file = $tujuan_upload.'/'.$nama_file;
-
-            $file->move($tujuan_upload, $nama_file);
-        }
-
-        $data_pendaftarInstrumen = PendaftarInstrumen::where([
-            'id_pendaftar' => $pendaftar->id,
-            'id_instrumen_asesmen' => $id_instrumen_asesmen
-        ])->first();
-
-        if($data_pendaftarInstrumen == null) {
-            PendaftarInstrumen::create([
-                'id_pendaftar' => $pendaftar->id,
-                'id_instrumen_asesmen' => $id_instrumen_asesmen,
-                'jawaban_self_asesmen' => $request->jawaban,
-                'path_bukti' => $path_file,
-                'komentar_bukti' => $request->komentar,
-                'created_by' => Auth::user()->username,
-                'edited_by' => Auth::user()->username
-            ]);
-        }
-        else {
-            PendaftarInstrumen::where('id', $data_pendaftarInstrumen->id)->update([
-                'jawaban_self_asesmen' => $request->jawaban,
-                'path_bukti' => $path_file,
-                'komentar_bukti' => $request->komentar,
-                'edited_by' => Auth::user()->username
-            ]);
-        }
-
-        return redirect()->route('asesi.self-asesmen.show-unit-kompetensi', ['id_sertifikasi' => $id_sertifikasi, 'id_ref_unit_kompetensi' => $id_ref_unit_kompetensi]);
+        return $pendaftar;
     }
 }
