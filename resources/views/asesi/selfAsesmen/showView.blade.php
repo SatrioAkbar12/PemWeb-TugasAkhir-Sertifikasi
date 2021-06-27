bd447aa5a5ef6477bc12547039993c4d7d0f6efd
SatrioAkbar12 <65120345+SatrioAkbar12@users.noreply.github.com>
2021-06-27 15:07:03 +0700

    update view asesi self asesmen

--- resources/views/asesi/selfAsesmen/showView.blade.php
+++ resources/views/asesi/selfAsesmen/showView.blade.php
@@ -16,45 +16,6 @@
     </section>
 
     <section class="content">
-        <!-- Default box -->
-        <!-- <div class="card">
-            <div class="card-body p-4">
-                <form method="post" action="/asesi/self-asesmen/{{ $data->id }}/jawab">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    {{-- {{ method_field('PUT') }} --}}
-
-                    <div class="form-group">
-                        <label for="input_jawaban_self_asesmen" class="form-label">Jawaban</label>
-                        <textarea id="input_jawaban_self_asesmen" class="form-control" name="jawaban_self_asesmen" type="text"
-                            placeholder="Masukkan jawaban ..." required >{{ $data->jawaban_self_asesmen }}</textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="input_path_bukti" class="form-label">Path Bukti</label>
-                        <textarea id="input_path_bukti" class="form-control" name="path_bukti" type="text"
-                            placeholder="Masukkan jawaban ..." required >{{ $data->path_bukti }}</textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="input_komentar_bukti" class="form-label">Komentar</label>
-                        <textarea id="input_komentar_bukti" class="form-control" name="komentar_bukti" type="text"
-                            placeholder="Masukkan jawaban ..." required >{{ $data->komentar_bukti }}</textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="input_jawaban_asesor_verifikasi" class="form-label">Jawaban Asesor Verifikasi</label>
-                        <textarea id="input_jawaban_asesor_verifikasi" class="form-control" name="jawaban_asesor_verifikasi" type="text"
-                            placeholder="Masukkan jawaban ..." required >{{ $data->jawaban_asesor_verifikasi }}</textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <button class="btn btn-primary" type="submit">Simpan</button>
-                        <a href="/asesi/self-asesmen"><button class="btn btn-danger" type="button">Kembali</button></a>
-                    </div>
-                </form>
-            </div>
-        </div> -->
-
         <table class="table table-striped projects">
             <tbody>
                 <tr>
@@ -101,9 +62,19 @@
                                         {{ $d->refUnitKompetensi->nama }}
                                     </div>
                                     <div class="col-3 text-right">
-                                        <a href="{{ route('asesi.self-asesmen.show-unit-kompetensi', ['id_sertifikasi' => $data->id, 'id_ref_unit_kompetensi' => $d->refUnitKompetensi->id]) }}">
-                                            <button class="btn btn-info" type="button">Jawab</button>
-                                        </a>
+                                        @foreach ($data_pendaftarinstrumen as $d_pendaftarinstrumen)
+                                            @if ($d_pendaftarinstrumen->instrumenAsesmenKompetensi->id_ref_unit_kompetensi == $d->id && $d_pendaftarinstrumen->jawaban_self_asesmen == null)
+                                                <a href="{{ route('asesi.self-asesmen.show-unit-kompetensi', ['id_sertifikasi' => $data->id, 'id_ref_unit_kompetensi' => $d->refUnitKompetensi->id]) }}">
+                                                    <button class="btn btn-info" type="button">Jawab</button>
+                                                </a>
+                                                @break
+                                            @elseif ($d_pendaftarinstrumen->instrumenAsesmenKompetensi->id_ref_unit_kompetensi == $d->id && $d_pendaftarinstrumen->jawaban_self_asesmen != null)
+                                                <a href="{{ route('asesi.self-asesmen.show-unit-kompetensi', ['id_sertifikasi' => $data->id, 'id_ref_unit_kompetensi' => $d->refUnitKompetensi->id]) }}">
+                                                    <button class="btn btn-info" type="button">Perbarui Jawaban</button>
+                                                </a>
+                                                @break
+                                            @endif
+                                        @endforeach
                                     </div>
                                 </div>
                             @endif
@@ -112,5 +83,13 @@
                 </tr>
             </tbody>
         </table>
+        <div class="py-3">
+            <a href="{{ route('asesi.self-asesmen.akhiri-asesmen', ['id_sertifikasi' => $data->id]) }}">
+                <button class="btn btn-primary" type="button">Simpan dan Akhiri</button>
+            </a>
+            <a href="{{ route('asesi.self-asesmen.index') }}">
+                <button class="btn btn-danger" type="button">Kembali</button>
+            </a>
+        </div>
     </section>
 @endsection
--- resources/views/asesi/selfAsesmen/show_unit_kompetensi.blade.php
+++ resources/views/asesi/selfAsesmen/show_unit_kompetensi.blade.php
@@ -10,7 +10,7 @@
         <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h4> Kompetensi : {{ $data->nama }}</h4>
+                <h4> Kompetensi : {{ $data_refunitkompetensi->nama }}</h4>
             </div>
         </div>
         </div><!-- /.container-fluid -->
@@ -20,33 +20,28 @@
         <!-- Default box -->
         <div class="card">
             <div class="card-body p-4">
-                <table class="table table-striped projects">
-                    <tbody>
-                        @foreach ($data->instrumenAsesmenKompetensi as $d)
-                            @if($d->is_aktif == 1)
-                                <tr>
-                                    <td>
-                                        {{ $d->instrumen_pertanyaan }}
-                                    </td>
-                                    <td class="text-right">
-                                        <a href="{{ route('asesi.self-asesmen.show-jawab', ['id_sertifikasi' => $id_sertifikasi, 'id_ref_unit_kompetensi' => $data->id, 'id_instrumen_asesmen' => $d->id])}}">
-                                            <button type="button" class="btn btn-primary">Jawab</button>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endif
-                        @endforeach
-                    </tbody>
-                </table>
-                <div class="py-3">
-                    <a href="{{ route('asesi.self-asesmen.lock-unit-kompetensi', ['id_sertifikasi' => $id_sertifikasi, 'id_ref_unit_kompetensi' => $data->id])}}">
+                <form method="post" action="{{ route('asesi.self-asesmen.store-asesmen', ['id_sertifikasi' => $id_sertifikasi, 'id_ref_unit_kompetensi' => $data_refunitkompetensi->id])}}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    @foreach ($data as $d)
+                        @if($d->instrumenAsesmenKompetensi->id_ref_unit_kompetensi == $data_refunitkompetensi->id && $d->instrumenAsesmenKompetensi->is_aktif == 1)
+                            <div class="py-3">
+                                <label for="pertanyaan_{{ $d->id_instrumen_asesmen }}" class="form-label">{{ $d->instrumenAsesmenKompetensi->instrumen_pertanyaan }}</label>
+                                <textarea id="pertanyaan_{{ $d->id_instrumen_asesmen }}" class="form-control" name="{{ $d->id_instrumen_asesmen }}_jawaban" placeholder="Masukkan jawaban ..." required>{{ $d->jawaban_self_asesmen }}</textarea>
+                                <div class="px-4 py-2">
+                                    <label class="from-label">Upload file</label>
+                                    <input id="pertanyaan_{{ $d->id_instrumen_asesmen }}" class="form-control" type="file" name="{{ $d->id_instrumen_asesmen }}_upload" value="{{ $d->path_bukti }}">
+                                    <textarea id="pertanyaan_{{ $d->id_instrumen_asesmen }}" class="form-control" name="{{ $d->id_instrumen_asesmen }}_uploadkomentar" placeholder="Komentar file upload ...">{{ $d->komentar_bukti }}</textarea>
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
+                    <div class="py-3">
                         <button class="btn btn-primary" type="submit">Simpan</button>
-                    </a>
-                    <a href="{{ route('asesi.self-asesmen.show-view', ['id_sertifikasi' => $id_sertifikasi]) }}">
-                        <button class="btn btn-danger" type="button">Kembali</button>
-                    </a>
-                    <p>*Tombol simpan akan mengunci unit kompetensi ini</p>
-                </div>
+                        <a href="{{ route('asesi.self-asesmen.show-view', ['id_sertifikasi' => $id_sertifikasi]) }}">
+                            <button class="btn btn-danger" type="button">Kembali</button>
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
     </section>
