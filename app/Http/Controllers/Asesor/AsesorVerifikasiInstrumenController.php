21587d4738459a3702b11a46bd1960e73bcbaf36
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-06-28 16:32:23 +0700

    update AsesorVerifikasiInstrumenController

--- app/Http/Controllers/Asesor/AsesorVerifikasiInstrumenController.php
+++ app/Http/Controllers/Asesor/AsesorVerifikasiInstrumenController.php
@@ -7,90 +7,68 @@ use Illuminate\Http\Request;
 use App\models\Asesor;
 use App\models\AsesorPendaftar;
 use App\models\AsesorJenisSertifikasi;
+use App\Models\InstrumenAsesmenKompetensi;
 use App\Models\Pendaftar;
 use App\Models\PendaftarInstrumen;
+use App\Models\RefUnitKompetensi;
+use App\Models\UnitKompetensiSertifikasi;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class AsesorVerifikasiInstrumenController extends Controller
 {
     public function index() {
-        $data_user = Auth::user()-> id;
-        $data_asesor = Asesor::where('id_user', $data_user)->first();
-        $data_asesor_jenis_sertifikasi = AsesorJenisSertifikasi::where('id_asesor', $data_asesor->id)->first();
-        $data_pendaftar = AsesorPendaftar::where('id_asesor_jenis_sertifikasi', $data_asesor_jenis_sertifikasi->id)->get();
+        $asesor_jenissertifikasi = AsesorJenisSertifikasi::where('id_asesor', Auth::user()->asesor->id)->first();
+        $data_pendaftar = AsesorPendaftar::where('id_asesor_jenis_sertifikasi', $asesor_jenissertifikasi->id)->get();
 
         return view('asesor.asesor.verifikasi_instrumen.show', ['data_pendaftar' => $data_pendaftar]);
     }
 
-    public function readPendaftar($id_asesorpendaftar) {
-        $data_pendaftar = AsesorPendaftar::where(['id' => $id_asesorpendaftar])->first();
-        $data_pendaftarinstrumen = PendaftarInstrumen::where('id_pendaftar', $data_pendaftar->id_pendaftar)->get();
+    public function readPendaftar($id_pendaftar) {
+        $data_pendaftar = Pendaftar::find($id_pendaftar);
+        $data_pendaftarinstrumen = PendaftarInstrumen::where('id_pendaftar', $id_pendaftar)->get();
+        $data_unitkompetensisertifikasi = UnitKompetensiSertifikasi::where('id_ref_jenis_sertifikasi', $data_pendaftar->penawaranSertifikasi->id_ref_jenis_sertifikasi)->get();
 
-        return view('asesor.asesor.verifikasi_instrumen.show_data', ['data' => $data_pendaftar, 'data_pendaftarinstrumen' => $data_pendaftarinstrumen, 'id_asesorpendaftar' => $id_asesorpendaftar]);
+        return view('asesor.asesor.verifikasi_instrumen.show_data', ['data' => $data_pendaftar, 'data_unitkompetensisertifikasi' => $data_unitkompetensisertifikasi,'data_pendaftarinstrumen' => $data_pendaftarinstrumen]);
     }
 
-    public function readInstrumen($id_asesorpendaftar, $id_instrumen) {
-        $data_asesorpendaftar = AsesorPendaftar::find($id_asesorpendaftar);
-        $data = PendaftarInstrumen::where([
-            'id_pendaftar' => $data_asesorpendaftar->id_pendaftar,
-            'id_instrumen_asesmen'=> $id_instrumen
-        ])->first();
+    public function readInstrumen($id_pendaftar, $id_ref_unit_kompetensi) {
+        $data = PendaftarInstrumen::where('id_pendaftar', $id_pendaftar)->get();
+        $data_refunitkompetensi = RefUnitKompetensi::find($id_ref_unit_kompetensi);
 
-        return view('asesor.asesor.verifikasi_instrumen.show_instrumen', ['data' => $data, 'id_asesorpendaftar' => $id_asesorpendaftar]);
+        return view('asesor.asesor.verifikasi_instrumen.show_instrumen', ['data' => $data, 'data_refunitkompetensi' => $data_refunitkompetensi, 'id_pendaftar' => $id_pendaftar]);
     }
 
-    public function showVerifikasi($id_asesorpendaftar, $id_instrumen) {
-        $data_asesorpendaftar = AsesorPendaftar::find($id_asesorpendaftar);
-        $data = PendaftarInstrumen::where([
-            'id_pendaftar' => $data_asesorpendaftar->id_pendaftar,
-            'id_instrumen_asesmen'=> $id_instrumen
-        ])->first();
+    public function verifikasi(Request $request, $id_pendaftar, $id_ref_unit_kompetensi) {
+        $data_pendaftarinstrumen = PendaftarInstrumen::where('id_pendaftar', $id_pendaftar)->get();
+        $data_instrumen = InstrumenAsesmenKompetensi::where('id_ref_unit_kompetensi', $id_ref_unit_kompetensi)->get();
 
-        return view('asesor.asesor.verifikasi_instrumen.form_jawaban', ['data' => $data, 'id_asesorpendaftar' => $id_asesorpendaftar, 'id_Instrumen' => $id_instrumen]);
+        foreach ($data_instrumen as $instrumen) {
+            $jawaban_asesor = $instrumen->id."_jawaban_asesor";
+
+            foreach ($data_pendaftarinstrumen as $d_pendaftarinstrumen){
+                if ($d_pendaftarinstrumen->id_instrumen_asesmen == $instrumen->id){
+                    PendaftarInstrumen::where([
+                        'id_pendaftar' => $id_pendaftar,
+                        'id_instrumen_asesmen' => $instrumen->id
+                    ])->update([
+                        'jawaban_asesor_verifikasi' => $request->$jawaban_asesor,
+                        'verified_by' => Auth::user()->username,
+                        'verified_at' => now()
+                    ]);
+                    break;
+                }
+            }
+        }
+
+        return redirect()->route('asesor.verifikasi-instrumen.read-pendaftar', ['id_pendaftar' => $id_pendaftar]);
     }
 
-    public function verifikasi($id_asesorpendaftar, $id_instrumen, Request $request) {
- 
+    public function akhiriVerifikasi($id_pendaftar) {
+        Pendaftar::where('id', $id_pendaftar)->update([
+            'status_akhir_sertifikasi' => 'selesai penilaian'
+        ]);
 
-        $data_asesorpendaftar = AsesorPendaftar::find($id_asesorpendaftar);
-
-    
-            PendaftarInstrumen::where([
-                'id_pendaftar' => $data_asesorpendaftar->id_pendaftar,
-                'id_instrumen_asesmen'=> $id_instrumen
-            ])->update([
-                'jawaban_asesor_verifikasi' => $request->jawaban,
-                'verified_by' => $data_asesorpendaftar->asesorJenisSertifikasi->asesor->nama,
-                'verified_at' => now(),
-                'edited_by' => Auth::user()->username
-            ]);
-    
-
-        // $this->cekAllVerified($id_asesorpendaftar);
-
-        return redirect()->route('asesor.verifikasi-instrumen.read-instrumen', ['id_asesorpendaftar' => $id_asesorpendaftar, 'id_instrumen' => $id_instrumen]);
+        return redirect()->route('asesor.verifikasi-instrumen.index');
     }
-
-    // public function cekAllVerified($id_asesorpendaftar) {
-    //     $data_asesorpendaftar = AsesorPendaftar::find($id_asesorpendaftar);
-
-    //     $data_pendaftarInstrumen = PendaftarSyarat::where([
-    //         'id_pendaftar' => $data_asesorpendaftar->id_pendaftar
-    //     ])->get();
-
-    //     $cek = 1;
-
-    //     foreach($data_pendaftarsyarat as $d) {
-    //         if($d->status_verifikasi_syarat != 'lolos verifikasi'){
-    //             $cek = 0;
-    //         }
-    //     }
-
-    //     if($cek == 1) {
-    //         Pendaftar::where('id', $data_asesorpendaftar->id_pendaftar)->update([
-    //             'status_akhir_sertifikasi' => 'siap asesmen'
-    //         ]);
-    //     }
-    // }
 }
